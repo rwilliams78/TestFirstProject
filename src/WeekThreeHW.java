10ecf5161a82f42ddd2fff4923a337af2df08127
rwilliams78 <rw31813@gmail.com>
2022-04-12 13:55:57 -0500

    Initial Commit: Basic structure and simple shape classes. Tests don't pass.

---
+++ src/WeekThreeHW.java
@@ -0,0 +1,17 @@
+public class WeekThreeHW {
+    public static void main(String args[]) {
+
+        String firstName = "rebecca williams";
+
+        System.out.println("First Name : " + firstName);
+
+        String concat = "R".concat("ebecca williams");
+        System.out.println("Capitalized first name :" +concat);
+
+        String fullName = "Rebecca_Williams";
+
+        System.out.println("Full Name : "+fullName.replace('_', ' '));
+
+    }
+
+}
